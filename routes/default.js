7df2f621cff52a9163519e6ba6cbfe38b7337e52
Alexandre Sauvage Potel <a.sauvagepotel@gmail.com>
2025-02-22 17:16:53 +0100

    [cleanup] use arrow function in callback

--- routes/default.js
+++ routes/default.js
@@ -2,25 +2,25 @@ const express = require('express');
 const router = express.Router();
 
 
-router.get('/', function (req, res) {
+router.get('/', (req, res) => {
     res.render('index', {
-        title: 'Homepage'
+        title: 'Homepage',
+        sessions: [{session_id: "250211-114807-25er623f"}, {session_id: "250211-114807-266e951a"}, {session_id: "250211-114807-59qe84rt"}]
     });
 
 });
 
-router.get('/about', function (req, res) {
+router.get('/about', (req, res) => {
     res.render('about', {
         title: 'About'
     });
 });
 
-router.get('/help', function (req, res) {
+router.get('/help', (req, res) => {
     res.render('help', {
         title: 'Help'
     });
 });
-
 
 
 module.exports = router;
--- routes/session.js
+++ routes/session.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 
-router.get('/', function (req, res) {
+router.get('/', (req, res) => {
     res.render('session', {
         title: 'Sessions'
     });
 });
 
-router.get('/:id', function (req, res) {
+router.get('/:id', (req, res) => {
     res.render('test', {
         title: 'Test ' + req.params.id,
         id: req.params.id
